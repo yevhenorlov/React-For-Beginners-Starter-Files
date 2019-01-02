6f3cffccfcac1e6fbb31f876de690f270340558a
yevhen orlov <yevhenorlov@gmail.com>
2019-01-02 20:34:36 +0200

    routing basics

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NotFound = () => <div className="not-found">404</div>
+
+export default NotFound
---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,29 @@
+// let's go!
+import React from 'react'
+import { getFunName } from '../helpers'
+
+class StorePicker extends React.Component {
+  myInput = React.createRef()
+  goToStore = e => {
+    e.preventDefault()
+    const storeName = this.myInput.current.value
+    this.props.history.push(`/store/${storeName}`)
+  }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter a Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store >>></button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker
